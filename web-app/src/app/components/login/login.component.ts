96fcc3374d0a19a84af9506332de1f03ac03fbd1
DESKTOP-IASUES3\abe <barbozabraham@gmail.com>
2022-12-15 08:41:01 -0800

    updated product components

--- web-app/src/app/components/login/login.component.ts
+++ web-app/src/app/components/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login-form',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username?: string;
+  password?: string;
+  errorMessage?: string;
 
+  constructor(private router: Router) {}
+
+  authenticate(form: NgForm) {
+    if (form.valid) {
+      //check username and password
+    }
+  }
 }
