641776b7425eb00a547093f149fd627dda81c244
Julio Canales <red.julio12@gmail.com>
2022-12-15 01:06:59 -0600

    fixed routerLink bug by implementing components

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -4,16 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductComponent } from './components/product/product.component';
-import {AuthComponent} from './components/authentication/auth.component'
+import { AuthComponent } from './components/authentication/auth.component'
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,AuthComponent
+    ProductComponent,
+    AuthComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CartComponent,
+    CheckoutComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
